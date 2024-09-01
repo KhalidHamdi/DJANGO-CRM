58664eea9eeb659ff0f2412b72d3f48fdd0dcf47
khalidhamdi <khalidhamdi204@gmail.com>
2024-09-01 09:56:04 +0300

    feat(register): implement registration with validation
    - Added form validation for required fields

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .form import SignUpForm
 
 def home(request):
     if request.method == "POST":
@@ -21,3 +22,19 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You have been logout successfully!")
     return redirect("home")
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"You have successfully Register")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})
