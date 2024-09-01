454dec626f29fe795190f666f408e864ba582036
khalidhamdi <khalidhamdi204@gmail.com>
2024-09-01 07:52:10 +0300

    feat(auth): implement login and logout functionality
    - Added user authentication with login and logout features

--- website/views.py
+++ website/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    if request.method == "POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request , username=username , password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "You have been Loged in successfully !")
+            return redirect('home')
+        else:
+            messages.success(request, "There was a problem trying login , Please Try again!")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logout successfully!")
+    return redirect("home")
